0c91d91c53b123a25aa0a77e31dfb5ecd301b9a6
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-01-14 13:08:04 +0600

    envo setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "flowbite-react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">Vite + React</h1>
+      <Button pill gradientDuoTone="tealToLime">CLick</Button>
+    </>
+  );
+}
+
+export default App;

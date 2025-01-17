faf7ae7505b10aca8c221a83cf63b6004a3b6835
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-01-17 16:59:55 +0600

    sign in and up input validation by hook form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,24 +1,33 @@
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
-    <div className="bg-gray-50 dark:bg-gray-900 mt-20">
+    <div className="mt-20">
       <Helmet>
         <title>Match Mate || Login</title>
       </Helmet>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-primary rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit(onSubmit)}
+            >
               <div>
-                <label
-                  for="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Your email
                 </label>
                 <input
@@ -26,25 +35,45 @@ const Login = () => {
                   name="email"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
+                  placeholder="example@gmail.com"
+                  {...register("email", { required: true })}
                 />
+                {errors.email && (
+                  <span className="text-red-600">Email is required</span>
+                )}
               </div>
               <div>
-                <label
-                  for="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Password
                 </label>
                 <input
                   type="password"
                   name="password"
                   id="password"
-                  placeholder="••••••••"
+                  placeholder="password"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  {...register("password", {
+                    required: "Password is required",
+                    maxLength: {
+                      value: 20,
+                      message: "Password not more than 20 character",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "Password not less than 6 characted",
+                    },
+                    pattern: {
+                      value: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* )/,
+                      message:
+                        "Password must contain one number, one uppercase, one lowercase and one special character",
+                    },
+                  })}
                 />
+                {errors.password && (
+                  <span className="text-red-600">
+                    {errors.password.message}
+                  </span>
+                )}
               </div>
               <div className="flex items-center justify-between">
                 <Link className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -12,25 +12,41 @@ const Signup = () => {
 
   const onSubmit = (data) => console.log(data);
   return (
-    <div className="bg-gray-50 dark:bg-gray-900 mt-20">
+    <div className="dark:bg-gray-900 mt-20">
       <Helmet>
         <title>Match Mate || Sign Up</title>
       </Helmet>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-primary rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Create your account
             </h1>
+            {/* inpur form  */}
             <form
-              className="space-y-4 md:space-y-6"
+              className="space-y-2 md:space-y-6"
               onSubmit={handleSubmit(onSubmit)}
             >
+              {/* name field  */}
               <div>
-                <label
-                  for="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Your name--"
+                  {...register("name", { required: "Name is required" })}
+                />
+                {errors.name && (
+                  <span className="text-red-600">{errors.name.message}</span>
+                )}
+              </div>
+              {/* email field  */}
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Your email
                 </label>
                 <input
@@ -38,32 +54,69 @@ const Signup = () => {
                   name="email"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
+                  placeholder="example@gmail.com"
+                  {...register("email", { required: "Email is required" })}
                 />
+                {errors.email && (
+                  <span className="text-red-600">{errors.email.message}</span>
+                )}
+              </div>
+              {/* photo url  */}
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Choose your photo
+                </label>
+                <input
+                  class="block w-full mb-2 text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                  id="small_size"
+                  type="file"
+                  {...register("file", {
+                    required: "Please choose your photo",
+                  })}
+                />
+                {errors.file && (
+                  <span className="text-red-600">{errors.file.message}</span>
+                )}
               </div>
               <div>
-                <label
-                  for="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Password
                 </label>
                 <input
                   type="password"
                   name="password"
                   id="password"
-                  placeholder="••••••••"
+                  placeholder="Password"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  {...register("password", {
+                    required: "Password is required",
+                    maxLength: {
+                      value: 20,
+                      message: "Password not more than 20 character",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "Password not less than 6 characted",
+                    },
+                    pattern: {
+                      value: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* )/,
+                      message:
+                        "Password must contain one number, one uppercase, one lowercase and one special character",
+                    },
+                  })}
                 />
+                {errors.password && (
+                  <span className="text-red-600">
+                    {errors.password.message}
+                  </span>
+                )}
               </div>
 
               <button type="submit" className="btn-normal w-full text-lg">
                 Sign Up
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
+                Already have an account?{" "}
                 <Link
                   to="/login"
                   className="font-medium text-accent hover:underline dark:text-primary-500"

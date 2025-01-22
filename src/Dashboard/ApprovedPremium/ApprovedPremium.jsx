a65774df5fffe9a73a2e293ae35fba38e28e7c58
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-01-23 02:20:01 +0600

    premium request accepted and update the user role

--- src/Dashboard/ApprovedPremium/ApprovedPremium.jsx
+++ src/Dashboard/ApprovedPremium/ApprovedPremium.jsx
@@ -3,6 +3,8 @@ import DashboardTitle from "../../Components/DashboardTitle/DashboardTitle";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { FaDeleteLeft } from "react-icons/fa6";
 import Swal from "sweetalert2";
+import { CiCircleCheck } from "react-icons/ci";
+import { FaRegCheckCircle } from "react-icons/fa";
 
 const ApprovedPremium = () => {
   const axiosSecure = useAxiosSecure();
@@ -46,10 +48,44 @@ const ApprovedPremium = () => {
     console.log(id);
   };
 
+  const handleAcceptPremiumRequest = (id, email) => {
+    Swal.fire({
+      title: "Are you sure?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes!",
+    })
+      .then((result) => {
+        if (result.isConfirmed) {
+          axiosSecure.patch(`/update/premiumRequest/${id}`).then((res) => {
+            console.log(res.data);
+
+            if (res.data.modifiedCount > 0) {
+              axiosSecure
+                .patch(`/user/role/${email}?role=premium`)
+                .then((res) => {
+                  console.log(res.data);
+                  if (res.data.modifiedCount > 0) {
+                    Swal.fire({
+                      title: "Done!",
+                      text: "Request accepted.",
+                      icon: "success",
+                    });
+                    refetch();
+                  }
+                });
+            }
+          });
+        }
+      })
+      .catch((err) => {});
+  };
   return (
     <div>
       <DashboardTitle
-        content={"Approved Premium"}
+        content={"Approve Premium"}
         qty={approvedPremium}
       ></DashboardTitle>
       <div>
@@ -122,7 +158,18 @@ const ApprovedPremium = () => {
                   <td className="px-6 py-4">{singlePremium?.biodataId}</td>
                   <td className="px-6 py-4">{singlePremium?.userEmail}</td>
                   <td className="px-6 py-4">
-                    <div>
+                    <div className="space-x-3">
+                      <button
+                        onClick={() =>
+                          handleAcceptPremiumRequest(
+                            singlePremium?.biodataId,
+                            singlePremium?.userEmail
+                          )
+                        }
+                        className="text-accent text-3xl"
+                      >
+                        <FaRegCheckCircle></FaRegCheckCircle>
+                      </button>
                       <button
                         onClick={() =>
                           handleApprovedPremium(singlePremium?.biodataId)
